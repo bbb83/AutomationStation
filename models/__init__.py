fb2830f443a030008b427f999cdd86101d3c7d8a
Bryce <111464939+bbb83@users.noreply.github.com>
2026-04-02 18:45:48 -0600

    Delete duplicate files

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#makes importing stuff easier
-
-from models.evidence import EvidenceRecord
-from models.device_record import DeviceRecord
-from models.scoring_result import TestResult, ScoringResult
-
-__all__=["EvidenceRecord", "DeviceRecord", "TestResult", "ScoringResult"]
